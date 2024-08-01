36336c2276329fe5b0790db84afdb12909ba78fe
Archer Bergmann <archerbergmann@The-Kraken.ht.home>
2024-08-01 15:55:13 -0400

    final

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
 
-const BookingForm = () => {
+const BookingForm = (props) => {
     const [date, setDate] = useState("");
     const [times, setTimes] = useState("");
-    const [guests, setGuest] = useState("");
+    const [guests, setGuests] = useState("");
     const [occasion, setOccasion] = useState("");
 
     const handleSubmit = (e) => {
@@ -31,10 +31,31 @@ const BookingForm = () => {
                         {/*for time selection*/}
                         <div>
                         <label htmlFor='book-time'>Coose Time:</label>
-                        <select id="book-time">
+                        <select id="book-time" value={times} onChange={(e) => setTimes(e.target.value)}>
                             <option value="">Select a Time</option>
+                            {
+                                props.availableTimes.availableTimes.map(availableTimes => {return <option key={availableTimes}>{availableTimes}</option>})
+                            }
                         </select>
                         </div>
+                            {/*for number of guests*/}
+                            <div>
+                                <label htmlFor='book-guests'>Number of Guests:</label>
+                                <input id='book-guests' min='1' value={guests} onChange={(e) => setGuests(e.target.value)}/>
+                            </div>
+
+                            {/*Occasion field*/}
+                            <div>
+                                <label htmlFor='book-occasion'>Occasion:</label>
+                                <select id='book-occasion' key={occasion} value={occasion} onChange={e => setOccasion(e.target.value)}>
+                                    <option>Birthday</option>
+                                    <option>Anniversary</option>
+                                </select>
+                            </div>
+                            {/*submit button*/}
+                            <div className='btnReceive'>
+                                <input aria-label='On Click' type='submit' value={"Make Your Reservation"} />
+                            </div>
                     </fieldset>
                 </form>
             </section>
---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const ConfirmedBooking = () => {
+    return (
+        <divc className='confirm'>
+            <div>
+                <h1>Booking has been <span>confirmed!</span></h1>
+            </div>
+        </divc>
+    );
+};
+
+
+export default ConfirmedBooking;
